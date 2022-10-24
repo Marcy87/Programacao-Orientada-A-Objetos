5886e263d2d7120dedcce7511cf1fb40ed414c65
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-24 18:27:27 -0300

    ConversaoDeMoeda
    Faça um programa para ler a cotação do dólar, e depois um valor em dólares a ser comprado por
    uma pessoa em reais. Informar quantos reais a pessoa vai pagar pelos dólares, considerando ainda
    que a pessoa terá que pagar 6% de IOF sobre o valor em dólar. Criar uma classe ConversorDeMoeda
    para ser responsável pelos cálculos.
    
    Exemplo:
    Qual é a cotação do dólar? 3.10
    Quantos dólares você vai comprar? 200.00
    Valor a ser pago em reais = 657.20

---
+++ ExerConvDeMoeda/ExerConvDeMoeda/ConversorDeMoeda.cs
@@ -0,0 +1,16 @@
+﻿namespace ExerConvDeMoeda
+{
+    class ConversorDeMoeda
+    {
+        public double cotacao;
+        public double dolar;
+
+        public static double Iof = 6.0;
+
+        public static double DolarParaReal(double cotacao, double dolar)
+        {
+            double calculo = cotacao * dolar;
+            return calculo + calculo * Iof / 100.0;
+        }
+    }
+}
---
+++ ExerConvDeMoeda/ExerConvDeMoeda/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerConvDeMoeda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double dolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = ConversorDeMoeda.DolarParaReal(cotacao, dolares);
+
+            Console.Write("Valor a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
