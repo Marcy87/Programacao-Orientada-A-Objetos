2a4d71821b89f6b2e6399d3ea9b890afa9d0bd50
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-24 16:39:56 -0300

    Funcionario
    Fazer um programa para ler os dados de um funcionário (nome,
    salário bruto e imposto). Em seguida, mostrar os dados do
    funcionário (nome e salário líquido). Em seguida, aumentar o salário
    do funcionário com base em uma porcentagem dada (somente o
    salário bruto é afetado pela porcentagem) e mostrar novamente os
    dados do funcionário. Use a classe projetada abaixo.
    
    Funcionario
    --------------------------------------------------------
    - Nom : string
    - SalarioBruto : double
    - Imposto : double
    --------------------------------------------------------
    + SalarioLiquido() : double
    + AumentarSalario(porcentagem : double) : void
    
    Exemplo:
    Nome: Joao Silva
    Salário bruto: 6000.00
    Imposto: 1000.00
    Funcionário: Joao Silva, $ 5000.00
    Digite a porcentagem para aumentar o salário: 10.0
    Dados atualizados: Joao Silva, $ 5600.00

---
+++ ExerFuncionario/ExerFuncionario/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario fun = new Funcionario();
+
+            Console.Write("Nome: ");
+            fun.Nome = Console.ReadLine();
+
+            Console.Write("Salário bruto: ");
+            fun.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Imposto: ");
+            fun.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionário: " + fun);
+
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            fun.AumentarSalario(porcentagem);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + fun);
+
+        }
+    }
+}
