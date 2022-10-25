0dd26d7b2836dd43c5320683d2f036210851991e
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-25 15:26:04 -0300

    ContaBancaria
    Em um banco, para se cadastrar uma conta bancária, é necessário informar o número da conta, o nome do
    titular da conta, e o valor de depósito inicial que o titular depositou ao abrir a conta. Este valor de depósito
    inicial, entretanto, é opcional, ou seja: se o titular não tiver dinheiro a depositar no momento de abrir sua
    conta, o depósito inicial não será feito e o saldo inicial da conta será, naturalmente, zero.
    Importante: uma vez que uma conta bancária foi aberta, o número da conta nunca poderá ser alterado. Já
    o nome do titular pode ser alterado (pois uma pessoa pode mudar de nome por ocasião de casamento, por
    exemplo).
    Por fim, o saldo da conta não pode ser alterado livremente. É preciso haver um mecanismo para proteger
    isso. O saldo só aumenta por meio de depósitos, e só diminui por meio de saques. Para cada saque
    realizado, o banco cobra uma taxa de $ 5.00. Nota: a conta pode ficar com saldo negativo se o saldo não for
    suficiente para realizar o saque e/ou pagar a taxa.
    Você deve fazer um programa que realize o cadastro de uma conta, dando opção para que seja ou não
    informado o valor de depósito inicial. Em seguida, realizar um depósito e depois um saque, sempre
    mostrando os dados da conta após cada operação.
    
    
    EXEMPLO 1
    Entre o número da conta: 8532
    Entre o titular da conta: Alex Green
    Haverá depósito inicial (s/n)? s
    Entre o valor de depósito inicial: 500.00
    Dados da conta:
    Conta 8532, Titular: Alex Green, Saldo: $ 500.00
    Entre um valor para depósito: 200.00
    Dados da conta atualizados:
    Conta 8532, Titular: Alex Green, Saldo: $ 700.00
    Entre um valor para saque: 300.00
    Dados da conta atualizados:
    Conta 8532, Titular: Alex Green, Saldo: $ 395.00
    
    
    EXEMPLO 2
    Entre o número da conta: 7801
    Entre o titular da conta: Maria Brown
    Haverá depósito inicial (s/n)? n
    Dados da conta:
    Conta 7801, Titular: Maria Brown, Saldo: $ 0.00
    Entre um valor para depósito: 200.00
    Dados da conta atualizados:
    Conta 7801, Titular: Maria Brown, Saldo: $ 200.00
    Entre um valor para saque: 198.00
    Dados da conta atualizados:
    Conta 7801, Titular: Maria Brown, Saldo: $ -3.00

---
+++ Banco/Banco/Conta.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+
+namespace Banco
+{
+    class Conta
+    {
+        //Atributos
+        public int Numero { get; private set; }  // Auto Properties
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }  // Auto Properties
+
+
+        //Construtor
+        public Conta(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public Conta(int numero, string titular, double depInic) : this (numero, titular)
+        {
+            Deposito(depInic);
+        }
+
+
+        //Métodos
+        public void Deposito(double deposito)
+        {
+            Saldo += deposito;
+        }
+
+        public void Saque(double saque)
+        {
+            Saldo -= saque + 5.0;
+        }
+
+
+
+        public override string ToString()
+        {
+            return "Conta "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Banco/Banco/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta;
+
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resposta = char.Parse(Console.ReadLine());
+
+
+            if (resposta == 's' || resposta == 'S')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depInic = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Conta(numero, titular, depInic);
+            } else
+            {
+                conta = new Conta(numero, titular);
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(deposito);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(saque);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta atualizadas:");
+            Console.WriteLine(conta);
+        }
+    }
+}
