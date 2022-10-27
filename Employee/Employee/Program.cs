251535b372be63eb486b601fb86b3adbe2ea7375
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-26 21:49:47 -0300

    Employee
    Fazer um programa para ler um número inteiro N e depois os dados (id, nome e salario) de
    N funcionários. Não deve haver repetição de id.
    Em seguida, efetuar o aumento de X por cento no salário de um determinado funcionário.
    Para isso, o programa deve ler um id e o valor X. Se o id informado não existir, mostrar uma
    mensagem e abortar a operação. Ao final, mostrar a listagem atualizada dos funcionários,
    conforme exemplos.
    Lembre-se de aplicar a técnica de encapsulamento para não permitir que o salário possa
    ser mudado livremente. Um salário só pode ser aumentado com base em uma operação de
    aumento por porcentagem dada.
    
    Exemplo:
    How many employees will be registered? 3
    Emplyoee #1:
    Id: 333
    Name: Maria Brown
    Salary: 4000.00
    Emplyoee #2:
    Id: 536
    Name: Alex Grey
    Salary: 3000.00
    Emplyoee #3:
    Id: 772
    Name: Bob Green
    Salary: 5000.00
    Enter the employee id that will have salary increase : 536
    Enter the percentage: 10.0
    Updated list of employees:
    333, Maria Brown, 4000.00
    536, Alex Grey, 3300.00
    772, Bob Green, 5000.00
    
    Exemplo:
    How many employees will be registered? 2
    Emplyoee #1:
    Id: 333
    Name: Maria Brown
    Salary: 4000.00
    Emplyoee #2:
    Id: 536
    Name: Alex Grey
    Salary: 3000.00
    Enter the employee id that will have salary increase: 776
    This id does not exist!
    Updated list of employees:
    333, Maria Brown, 4000.00
    536, Alex Grey, 3000.00

---
+++ Employee/Employee/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee;
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("How many employees will be registered? ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine("Employee #" + i + ":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                list.Add(new Employee(id, name, salary));
+
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter the employee id that will have salary increase : ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            Employee empl = list.Find(x => x.Id == searchId);
+
+            if (empl != null)
+            {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                empl.IncreaseSalary(percentage);
+            }
+            else
+            {
+                Console.WriteLine("This is does not exist!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees:");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp);
+            }
+;
+        }
+    }
+}
